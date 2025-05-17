a091a55eda39fc3b87849003e6c288999fa48c10
Alejandro Gispert <alejandrobusiness2022@gmail.com>
2025-05-17 20:09:43 +0200

    feat: provinces, more

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,5 +1,3 @@
-// src/constants/filters.ts
-
 export const yearRange: [number, number] = [1800, new Date().getFullYear()];
 
 export const allMusicStyles: string[] = [
@@ -35,13 +33,136 @@ export const allProvinces: string[] = [
 ];
 
 export const allCities: string[] = [
+  // Havana (La Habana)
   "Havana",
-  "Santiago de Cuba",
-  "Holguín",
-  "Camagüey",
-  "Santa Clara",
+  "Arroyo Naranjo",
+  "Boyeros",
+  "Centro Habana",
+  "Cerro",
+  "Cotorro",
+  "Diez de Octubre",
+  "Guanabacoa",
+  "La Habana del Este",
+  "La Habana Vieja",
+  "La Lisa",
+  "Marianao",
+  "Playa",
+  "Plaza de la Revolución",
+  "Regla",
+  "San Miguel del Padrón",
+
+  // Artemisa
+  "Artemisa",
+  "Bauta",
+  "Caimito",
+  "Guanajay",
+  "Güira de Melena",
+  "Mariel",
+  "San Antonio de los Baños",
+  "San Cristóbal",
+
+  // Mayabeque
+  "San José de las Lajas",
+  "Batabanó",
+  "Bejucal",
+  "Güines",
+  "Jaruco",
+  "Melena del Sur",
+  "Nueva Paz",
+  "Quivicán",
+  "Santa Cruz del Norte",
+
+  // Matanzas
   "Matanzas",
-  "Bayamo",
+  "Cárdenas",
+  "Colón",
+  "Jovellanos",
+  "Perico",
+  "Unión de Reyes",
+  "Los Arabos",
+  "Jagüey Grande",
+
+  // Villa Clara
+  "Santa Clara",
+  "Caibarién",
+  "Camajuaní",
+  "Placetas",
+  "Remedios",
+  "Sagua la Grande",
+  "Manicaragua",
+
+  // Cienfuegos
   "Cienfuegos",
-  "Pinar del Río",
+  "Cumanayagua",
+
+  // Sancti Spíritus
+  "Sancti Spíritus",
+  "Cabaiguán",
+  "Fomento",
+  "Jatibonico",
+  "Trinidad",
+
+  // Ciego de Ávila
+  "Ciego de Ávila",
+  "Morón",
+  "Baraguá",
+  "Chambas",
+  "Ciro Redondo",
+  "Florencia",
+  "Majagua",
+  "Venezuela",
+
+  // Camagüey
+  "Camagüey",
+  "Florida",
+  "Nuevitas",
+  "Vertientes",
+  "Esmeralda",
+  "Sibanicú",
+  "Santa Cruz del Sur",
+
+  // Las Tunas
+  "Las Tunas",
+  "Amancio",
+  "Colombia",
+  "Jobabo",
+  "Majibacoa",
+  "Puerto Padre",
+
+  // Holguín
+  "Holguín",
+  "Banes",
+  "Gibara",
+  "Mayarí",
+  "Moa",
+  "Sagua de Tánamo",
+  "San Germán",
+
+  // Granma
+  "Bayamo",
+  "Manzanillo",
+  "Jiguaní",
+  "Niquero",
+  "Media Luna",
+  "Yara",
+  "Guisa",
+
+  // Santiago de Cuba
+  "Santiago de Cuba",
+  "Contramaestre",
+  "Palma Soriano",
+  "San Luis",
+  "Songo-La Maya",
+  "La Maya",
+
+  // Guantánamo
+  "Guantánamo",
+  "Baracoa",
+  "Imías",
+  "Maisí",
+  "Niceto Pérez",
+  "San Antonio del Sur",
+
+  // Isla de la Juventud
+  "Nueva Gerona",
 ];
